c89fe65203024ec482bbbde57b27c321c16429f3
Alessandro Isaacs <dr.ahfz@gmail.com>
2015-06-08 15:44:40 -0700

    the app

---
+++ app.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+var responder = function (req, res) {
+  var code = parseInt(req.params.requestedCode);
+  console.log('Responding with code: ', code);
+  res.sendStatus(code);
+};
+
+app.get('/:requestedCode', responder);
+app.post('/:requestedCode', responder);
+app.put('/:requestedCode', responder);
+
+var server = app.listen(5000, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Request Echo listening at http://%s:%s', host, port);
+
+});
